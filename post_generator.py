c454310368451c62cebc53dc1487b90e190e6010
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-06-30 15:31:54 +1200

    Add files via upload

--- post_generator.py
+++ post_generator.py
@@ -13,7 +13,7 @@ if data[len(data) - 1] == ",":
 data = data.split(",")
 
 # Add category title
-text = "[size=150]" + input("Enter category/AoE/strength: ") + "[/size]\n\n"
+text = "[size=150]" + input("Enter category/AoE/strength: ") + "[/size]\n\n[spoiler=Data]"
 
 # Loop through each stat
 item = 9
@@ -28,14 +28,14 @@ while item < len(data):
 			# Highlight stat name if correlation is exceptionally strong
 			text += "[color=green][b]" + data[item] + "[/b][/color]\n"
 		else:
-			text += "[b]" + data[item] + "[/b]"
-		text += "[list][*]Mean: " + data[item + 1] + "\n[*]Standard Deviation: " + data[item + 2] + "\n[*]Skewness: " + data[item + 3] + "\n[*]Maximum: " + data[item + 4] + "\n[*]Minimum: " + data[item + 5] + "[/list]\n"
+			text += "[b]" + data[item] + "[/b]\n"
+		text += "[list][*]Mean: " + data[item + 1] + "[*]Standard Deviation: " + data[item + 2] + "[*]Skewness: " + data[item + 3] + "[*]Maximum: " + data[item + 4] + "[*]Minimum: " + data[item + 5] + "[/list]\n"
 		
 		# Include linear relationship data if p-value is below 0.05
 		if eval(data[item + 6]) < 0.05:
-			text += "Linear relationship data (NB: Linear model may be inaccurate),[list][*]P-Value: " + data[item + 6] + "\n[*]Slope: " + data[item + 7] + "\n[*]Intercept: " + data[item + 8] + "[/list]\n"
+			text += "Linear relationship data (NB: Linear model may be inaccurate),[list][*]P-Value: " + data[item + 6] + "[*]Slope: " + data[item + 7] + "[*]Intercept: " + data[item + 8] + "[/list]\n"
 	item += 9
-
+text += "[/spoiler]"
 # Print text content
 print(text)
 time.sleep(30) # Wait before ending script to give reasonable time to copy content, if not script is for some reason not run from command line
--- process_dumps.py
+++ process_dumps.py
@@ -5,6 +5,7 @@ import os
 
 # Get inputs
 resolution = input('Enter GA resolution ID (eg 654 for GA #654): ')
+fname = input('Enter file name (exclude extension): ')
 user_name = input('Enter your nation name: ')
 
 # Get resolution passage date
@@ -22,13 +23,14 @@ if not os.path.exists('./stats'):
 
 # Initiate stats CSV file
 print('Initiating data file')
-f = open('stats/' + resolution + '.csv', 'w')
-f.write('Nation,s0,f0,s1,f1,s2,f2,s4,f4,s5,f5,s6,f6,s7,f7,s8,f8,s9,f9,s10,f10,s11,f11,s12,f12,s13,f13,s14,f14,s15,f15,s16,f16,s17,f17,s18,f18,s19,f19,s20,f20,s21,f21,s22,f22,s23,f23,s24,f24,s25,f25,s26,f26,s27,f27,s28,f28,s29,f29,s30,f30,s31,f31,s32,f32,s33,f33,s34,f34,s35,f35,s36,f36,s37,f37,s38,f38,s39,f39,s40,f40,s41,f41,s42,f42,s43,f43,s44,f44,s45,f45,s46,f46,s47,f47,s48,f48,s49,f49,s50,f50,s51,f51,s52,f52,s53,f53,s54,f54,s55,f55,s56,f56,s57,f57,s58,f58,s59,f59,s60,f60,s61,f61,s62,f62,s63,f63,s64,f64,s67,f67,s68,f68,s69,f69,s70,f70,s71,f71,s72,f72,s73,f73,s74,f74,s75,f75,s77,f77,s78,f78,s79,f79,s85,f85,s87,f87,s88,f88\n')
-f.close()
+if not os.path.exists('stats/' + fname + '.csv'):
+	f = open('stats/' + fname + '.csv', 'w')
+	f.write('Nation,s0,f0,s1,f1,s2,f2,s4,f4,s5,f5,s6,f6,s7,f7,s8,f8,s9,f9,s10,f10,s11,f11,s12,f12,s13,f13,s14,f14,s15,f15,s16,f16,s17,f17,s18,f18,s19,f19,s20,f20,s21,f21,s22,f22,s23,f23,s24,f24,s25,f25,s26,f26,s27,f27,s28,f28,s29,f29,s30,f30,s31,f31,s32,f32,s33,f33,s34,f34,s35,f35,s36,f36,s37,f37,s38,f38,s39,f39,s40,f40,s41,f41,s42,f42,s43,f43,s44,f44,s45,f45,s46,f46,s47,f47,s48,f48,s49,f49,s50,f50,s51,f51,s52,f52,s53,f53,s54,f54,s55,f55,s56,f56,s57,f57,s58,f58,s59,f59,s60,f60,s61,f61,s62,f62,s63,f63,s64,f64,s67,f67,s68,f68,s69,f69,s70,f70,s71,f71,s72,f72,s73,f73,s74,f74,s75,f75,s77,f77,s78,f78,s79,f79,s85,f85,s87,f87,s88,f88')
+	f.close()
 saved_dumps = []
 
 original_time = time.time_ns() - 650000000
-text = ''
+text = '\n'
 
 # Loop through stats on nation
 for nation in nats:
@@ -53,6 +55,6 @@ for nation in nats:
 
 # Save data
 print ('Saving data')
-f = open('stats/' + resolution + '.csv', 'a')
+f = open('stats/' + fname + '.csv', 'a')
 f.write(text)
 f.close()
