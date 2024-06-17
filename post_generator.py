5d38fc813870529c389890c7ed204124fde8baa8
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2024-06-17 18:25:30 +1200

    Revert highlight cutoff to 30

--- post_generator.py
+++ post_generator.py
@@ -18,28 +18,32 @@ text = "[size=150]" + category + "[/size]\n\n[spoiler=Data]"
 
 # Loop through each stat
 item = 9
-
 while item < len(data):
-	print("Processing " + data[item] + "...")
-	if data[item + 4] == "0" and data[item + 5] == "0":
-		# Assume no data exists if both max and min are zero
-		pass
-	else:
-		if eval(data[item + 1]) > 25 and eval(data[item + 5]) >= eval(data[item + 1]) * -0.05:
-			# Highlight in green if increase is exceptionally strong 
-			text += "[color=green][b]" + data[item] + "[/b][/color]\n"
-		elif eval(data[item + 1]) < -25 and eval(data[item + 4]) <= eval(data[item + 1]) * -0.05:
-			# Highlight in red if decrease is exceptionally strong
- 			text += "[color=red][b]" + data[item] + "[/b][/color]\n"
+	try:
+		if data[item + 4] == "0" and data[item + 5] == "0":
+			# Assume no data exists if both max and min are zero
+			pass
 		else:
-			text += "[b]" + data[item] + "[/b]\n"
-		text += "[list][*]Mean: " + data[item + 1] + "[*]Standard Deviation: " + data[item + 2] + "[*]Skewness: " + data[item + 3] + "[*]Maximum: " + data[item + 4] + "[*]Minimum: " + data[item + 5] + "[/list]\n"
-		
-		# Include linear relationship data if p-value is below 0.05
-		if eval(data[item + 6]) < 0.05:
-			text += "Linear relationship data (NB: Linear model may be inaccurate),[list][*]P-Value: " + data[item + 6] + "[*]Slope: " + data[item + 7] + "[*]Intercept: " + data[item + 8] + "[/list]\n"
+			if eval(data[item + 1]) > 30 and eval(data[item + 5]) >= eval(data[item + 1]) * -0.05:
+				# Highlight in green if increase is exceptionally strong 
+				text += "[color=green][b]" + data[item] + "[/b][/color]\n"
+			elif eval(data[item + 1]) < -30 and eval(data[item + 4]) <= eval(data[item + 1]) * -0.05:
+				# Highlight in red if decrease is exceptionally strong
+ 				text += "[color=red][b]" + data[item] + "[/b][/color]\n"
+			else:
+				text += "[b]" + data[item] + "[/b]\n"
+			text += "[list][*]Mean: " + data[item + 1] + "[*]Standard Deviation: " + data[item + 2] + "[*]Skewness: " + data[item + 3] + "[*]Maximum: " + data[item + 4] + "[*]Minimum: " + data[item + 5] + "[/list]\n"
+			
+			# Include linear relationship data if p-value is below 0.05
+			if eval(data[item + 6]) < 0.05:
+				text += "Linear relationship data (NB: Linear model may be inaccurate),[list][*]P-Value: " + data[item + 6] + "[*]Slope: " + data[item + 7] + "[*]Intercept: " + data[item + 8] + "[/list]\n"
+	except:
+		print("Error in processing " + data[item])
+		raise
+	
 	item += 9
 text += "[/spoiler]"
+
 # Print text content
 print(text)
 time.sleep(30) # Wait before ending script to give reasonable time to copy content, if script is for some reason not run from command line
